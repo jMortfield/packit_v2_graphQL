09d119d8b9879de820364a38b74c53a14ccbfb62
Jason <j.mortfield44@gmail.com>
2019-02-24 19:51:06 -0500

    Began to create a schema. Added info to README file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # packit_v2_graphQL
+
+This is a project that uses Node.js, Express, GraphQL, MongoDB and React. The purpose of this app is to help users generate, and store, a packing list for their travels, based on criteria entered by the user. The user is able to edit the list as they please, and log in in order to save it for later use.
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,1 +1,31 @@
 const graphql = require('graphql');
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList
+} = graphql;
+
+// Users will filter list by TripType, Temperature, and Duration
+
+const ItemType = new GraphQLObjectType({
+    name: 'Item',
+    fields: () => ({
+        name: {type: GraphQLString },
+        itemType: {type: GraphQLString},
+        temp: {type: GraphQLString},
+        qty: {type: GraphQLInt}
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        items: {
+            type: new GraphQLList(ItemType),
+        }
+    }
+})
